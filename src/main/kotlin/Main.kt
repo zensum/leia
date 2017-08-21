592ff7e4b7cf15d8b9271a99d3c427780acce8b4
Anton Österberg <anton.osterberg@gmail.com>
2017-08-21 17:44:14 +0200

    Works as intended now.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,37 +6,57 @@ import org.jetbrains.ktor.content.HttpStatusCodeContent
 import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.request.path
 import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.routing.route
 import org.jetbrains.ktor.routing.routing
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    server(1234).start(true)
+    val port: Int = Integer.parseInt(getEnv("PORT", "80"))
+    server(port).start(true)
 }
 
+fun getEnv(e : String, default: String? = null) : String = System.getenv()[e] ?: default ?: throw RuntimeException("Missing environment variable $e and no default value is given.")
+
 private val producer = ProducerBuilder.ofString.create()
+private val routesFile = getEnv("ROUTES_FILE", "/etc/config/routes")
 
 fun server(port: Int) = embeddedServer(Netty, port) {
+    val routes = parseRoutesFile(routesFile)
+
+    if(routes.isEmpty()) {
+        System.err.println("No routes found in $routesFile")
+        System.exit(1)
+    }
+
     routing {
-        route("*") {
-            handle {
-                val path = call.request.path()
-                if (path in routes.keys) {
+        for((path, topic) in routes) {
+            route(path) {
+                handle {
                     val request: PayLoad = createPayload(this)
                     println(request)
-                    val topic: String = routes[path]!!
                     producer.sendRaw(ProducerRecord(topic, request.toString()))
                     call.respond(HttpStatusCodeContent(HttpStatusCode.OK))
-                }
-                else {
-                    call.respond(HttpStatusCodeContent(HttpStatusCode.NotFound))
                 }
             }
         }
     }
 }
 
-private val routes = mapOf("/status" to "status",
-    "/" to "all",
-    "/test" to "test")
+fun parseRoutesFile(file: String): Map<String, String> {
+    val path: Path = Paths.get(file)
+    return Files.readAllLines(path).asSequence()
+        .map { lineToPair(it) }
+        .toMap()
+}
+
+fun lineToPair(line: String): Pair<String, String> {
+    val delimiter = Regex("\\s*->\\s*")
+    val split: List<String> = line.split(delimiter)
+    if(split.size != 2)
+        throw IllegalArgumentException("Found invalid route entry: $line")
+
+    return Pair(split[0], split[1])
+}
---
+++ src/test/kotlin/ParseRouteFileTest.kt
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import se.zensum.webhook.lineToPair
+import se.zensum.webhook.parseRoutesFile
+import java.lang.IllegalArgumentException
+
+class someTest {
+
+    @Test
+    fun testReadFile() {
+        val routes: Map<String, String> = parseRoutesFile("src/test/routes")
+        assertEquals(4, routes.size)
+
+        assertEquals("test", routes["/test"])
+        assertEquals("mail", routes["/status/mail"])
+        assertEquals("sms", routes["/status/sms"])
+        assertEquals("banks", routes["/bank/{bank}"])
+    }
+
+    @Test
+    fun testLineToPair() {
+        val line1 = "/test -> test"
+        val line2 = "/bank/{bank} -> banks"
+
+        val (key1, value1) = lineToPair(line1)
+        assertEquals("/test", key1)
+        assertEquals("test", value1)
+
+        val (key2, value2) = lineToPair(line2)
+        assertEquals("/bank/{bank}", key2)
+        assertEquals("banks", value2)
+    }
+
+    @Test
+    fun testLineToPairWithExtraWhiteSpace() {
+        val line1 = "/test  ->  test"
+        val line2 = "/bank/{bank}   ->   banks"
+
+        val (key1, value1) = lineToPair(line1)
+        assertEquals("/test", key1)
+        assertEquals("test", value1)
+
+        val (key2, value2) = lineToPair(line2)
+        assertEquals("/bank/{bank}", key2)
+        assertEquals("banks", value2)
+    }
+
+    @Test
+    fun testLineToPairWithNoWhiteSpace() {
+        val line1 = "/test->test"
+        val line2 = "/bank/{bank}->banks"
+
+        val (key1, value1) = lineToPair(line1)
+        assertEquals("/test", key1)
+        assertEquals("test", value1)
+
+        val (key2, value2) = lineToPair(line2)
+        assertEquals("/bank/{bank}", key2)
+        assertEquals("banks", value2)
+    }
+
+    @Test
+    fun testLineToPairWithMissingDelimiter() {
+        assertThrows(IllegalArgumentException::class.java) {
+            lineToPair("/test-test")
+        }
+    }
+
+    @Test
+    fun testLineToPairWithMultipleDelimiters() {
+        assertThrows(IllegalArgumentException::class.java) {
+            lineToPair("/test->test1->test2")
+        }
+    }
+}
