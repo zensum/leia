5aa7dc098c9e7dc15e2861ee95404e82654bcc69
Anton Österberg <anton.osterberg@gmail.com>
2017-09-18 11:02:36 +0200

    Rewrote test cases to do more indepndent testing of functionality.

--- src/test/kotlin/ParseRouteFileTest.kt
+++ src/test/kotlin/ParseRouteFileTest.kt
@@ -10,42 +10,111 @@ import se.zensum.webhook.httpMethods
 
 class ParseRoutesTest {
 
+    private val routes: Map<String, TopicRouting> = getRoutes("src/test/routes")
+
     @Test
-    fun testReadFile() {
-        val routes: Map<String, TopicRouting> = getRoutes("src/test/routes")
+    fun testSize() {
+        assertEquals(4, routes.size)
+    }
+
+    @Test
+    fun testTopic() {
+        routes["/status/mail"]!!.apply{
+            assertEquals("mail-status", topic)
+        }
+
+        routes["/status/sms"]!!.apply{
+            assertEquals("sms-status", topic)
+        }
+
+        routes["/test"]!!.apply {
+            assertEquals("test", topic)
+        }
+
+        routes["/auth"]!!.apply {
+            assertEquals("test", topic)
+        }
+    }
+
+    @Test
+    fun testPath() {
+        routes["/status/mail"]!!.apply{
+            assertEquals("/status/mail", path)
+        }
+
+        routes["/status/sms"]!!.apply{
+            assertEquals("/status/sms", path)
+        }
+
+        routes["/test"]!!.apply {
+            assertEquals("/test", path)
+        }
+
+        routes["/auth"]!!.apply {
+            assertEquals("/auth", path)
+        }
+    }
+
+    @Test
+    fun testVerify() {
+        routes["/status/mail"]!!.apply{
+            assertFalse(verify)
+        }
+
+        routes["/status/sms"]!!.apply{
+            assertFalse(verify)
+        }
+
+        routes["/test"]!!.apply {
+            assertFalse(verify)
+        }
+
+        routes["/auth"]!!.apply {
+            assertTrue(verify)
+        }
+    }
+
+    @Test
+    fun testFormat() {
         assertEquals(4, routes.size)
 
         routes["/status/mail"]!!.apply{
-            assertEquals("mail-status", topic)
-            assertEquals("/status/mail", path)
-            assertFalse(verify)
             assertEquals(Format.PROTOBUF, format)
+        }
+
+        routes["/status/sms"]!!.apply{
+            assertEquals(Format.PROTOBUF, format)
+        }
+
+        routes["/test"]!!.apply {
+            assertEquals(Format.PROTOBUF, format)
+        }
+
+        routes["/auth"]!!.apply {
+            assertEquals(Format.RAW_BODY, format)
+        }
+    }
+
+    @Test
+    fun testExpectedMethods() {
+        assertEquals(4, routes.size)
+
+        routes["/status/mail"]!!.apply{
             val expectedMethods = setOf(HttpMethod.Post, HttpMethod.Put, HttpMethod.Head, HttpMethod.Get)
             assertEquals(expectedMethods, allowedMethods)
         }
 
         routes["/status/sms"]!!.apply{
-            assertEquals("sms-status", topic)
-            assertEquals("/status/sms", path)
-            assertFalse(verify)
-            assertEquals(Format.PROTOBUF, format)
             assertEquals(httpMethods.verbs, allowedMethods)
         }
 
         routes["/test"]!!.apply {
-            assertEquals("test", topic)
-            assertEquals("/test", path)
-            assertFalse(verify)
-            assertEquals(Format.PROTOBUF, format)
             assertEquals(httpMethods.verbs, allowedMethods)
         }
 
         routes["/auth"]!!.apply {
-            assertEquals("test", topic)
-            assertEquals("/auth", path)
-            assertTrue(verify)
             val expectedMethods = setOf(HttpMethod.Post)
-            assertEquals(expectedMethods, format)
+            assertEquals(expectedMethods, allowedMethods)
         }
     }
 }
