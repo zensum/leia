fd6268b174eb93cf612bf4b939166fad3a4f1988
William Rudenmalm <me@whn.se>
2018-07-12 18:48:39 +0200

    Add custom exception class for not being able to resolve sinks

--- src/main/kotlin/sink/SinkProvider.kt
+++ src/main/kotlin/sink/SinkProvider.kt
@@ -3,12 +3,16 @@ package leia.sink
 import leia.logic.IncomingRequest
 import leia.logic.SinkDescription
 
+class SinkNotFoundException(val sinkDescription: SinkDescription) :
+    Exception("Unable to find sink for description $sinkDescription") {
+}
+
 interface SinkProvider {
     fun sinkFor (description: SinkDescription): Sink?
 
     // Convenience method for getting a sink and handling a request
     suspend fun handle(description: SinkDescription, req: IncomingRequest) =
-        (sinkFor(description) ?: throw NullPointerException("Unable to find sink for description $description")).handle(req)
+        (sinkFor(description) ?: throw SinkNotFoundException(description)).handle(req)
 }
 
 
