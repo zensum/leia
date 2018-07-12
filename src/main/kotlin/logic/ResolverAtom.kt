a0bbf4e7e7aeefe0a4c7c60653a17ac66299ae4a
William Rudenmalm <me@whn.se>
2018-07-12 18:44:26 +0200

    Fix issue with Atoms not implementing the interface they are supposed to

--- src/main/kotlin/logic/ResolverAtom.kt
+++ src/main/kotlin/logic/ResolverAtom.kt
@@ -2,9 +2,11 @@ package leia.logic
 
 import leia.Atom
 
-class ResolverAtom(private var inner: Resolver) : Resolver by inner, Atom<Resolver> {
+class ResolverAtom(private var inner: Resolver) : Resolver, Atom<Resolver> {
     override fun set(new: Resolver) {
         inner = new
     }
+
+    override fun resolve(req: IncomingRequest): Result = inner.resolve(req)
 }
 
--- src/main/kotlin/sink/SinkProviderAtom.kt
+++ src/main/kotlin/sink/SinkProviderAtom.kt
@@ -1,9 +1,15 @@
 package leia.sink
 
 import leia.Atom
+import leia.logic.IncomingRequest
+import leia.logic.SinkDescription
 
-class SinkProviderAtom(private var cur: SinkProvider) : SinkProvider by cur, Atom<SinkProvider> {
+class SinkProviderAtom(private var cur: SinkProvider) : SinkProvider, Atom<SinkProvider> {
     override fun set(new: SinkProvider) {
         cur = new
     }
+    override fun sinkFor(description: SinkDescription): Sink? = cur.sinkFor(description)
+    override suspend fun handle(description: SinkDescription, req: IncomingRequest): SinkResult =
+        cur.handle(description, req)
+
 }
