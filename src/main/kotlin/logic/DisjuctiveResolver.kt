5ee5746172a4f4328c23941cf10d2b91b113adeb
William Rudenmalm <me@whn.se>
2018-07-12 18:49:58 +0200

    Handle zero element case for disjunctive resolver

--- src/main/kotlin/logic/DisjuctiveResolver.kt
+++ src/main/kotlin/logic/DisjuctiveResolver.kt
@@ -2,10 +2,11 @@ package leia.logic
 
 class DisjunctiveResolver(private val resolvers: List<Resolver>) : Resolver {
     override fun resolve(req: IncomingRequest): Result =
-        resolvers.asSequence().map {
+        if (resolvers.isEmpty()) {
+            NoMatch
+        } else resolvers.asSequence().map {
             it.resolve(req)
         }.reduce { acc, result ->
             acc.combine(result)
         }
 }
-
