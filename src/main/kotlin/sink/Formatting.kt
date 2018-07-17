d5f66d67dd5b26ab8f285ba9826be4b070eccc25
William Rudenmalm <me@whn.se>
2018-07-17 23:02:33 +0200

    Split formatting of responses from Kafka logic

---
+++ src/main/kotlin/sink/Formatting.kt
@@ -0,0 +1,52 @@
+package leia.sink
+
+import leia.logic.IncomingRequest
+import se.zensum.leia.config.Format
+import io.ktor.http.HttpMethod as KtorHttpMethod
+import se.zensum.webhook.PayloadOuterClass
+
+typealias HttpMethod = PayloadOuterClass.HttpMethod
+fun convertMethod(method: KtorHttpMethod): PayloadOuterClass.HttpMethod = when(method) {
+    KtorHttpMethod.Put -> HttpMethod.PUT
+    KtorHttpMethod.Patch -> HttpMethod.PATCH
+    KtorHttpMethod.Delete -> HttpMethod.DELETE
+    KtorHttpMethod.Get -> HttpMethod.GET
+    KtorHttpMethod.Head -> HttpMethod.HEAD
+    KtorHttpMethod.Post -> HttpMethod.POST
+    KtorHttpMethod.Options -> HttpMethod.OPTIONS
+    else -> throw IllegalArgumentException("Method ${method.value} is not supported")
+}
+
+fun toProtoPair(pair: Pair<String, String>): PayloadOuterClass.MultiMap.Pair {
+    val (k, v) = pair
+    return PayloadOuterClass.MultiMap.Pair.newBuilder().also {
+        it.key = k
+        it.value = v
+    }.build()
+}
+
+fun parseMap(valuesMap: Map<String, List<String>>): PayloadOuterClass.MultiMap {
+    return PayloadOuterClass.MultiMap.newBuilder().apply {
+        valuesMap.entries.asSequence()
+            .flatMap { (k, vs) -> vs.map { v -> k to v }.asSequence() }
+            .map { toProtoPair(it) }
+            .forEach { addPair(it) }
+    }.build()
+}
+
+suspend fun createPayloadProtobuf(req: IncomingRequest): PayloadOuterClass.Payload =
+    req.run {
+        PayloadOuterClass.Payload.newBuilder().also {
+            it.path = path
+            it.method = convertMethod(method)
+            it.headers = parseMap(headers)
+            it.queryString = queryString
+            it.body = readBody().toString(Charsets.UTF_8)
+            it.flakeId = requestId
+        }.build()
+    }
+internal suspend fun Format.generateBody(req: IncomingRequest) : ByteArray = when (this) {
+    Format.RAW_BODY -> req.readBody()
+    Format.PROTOBUF -> createPayloadProtobuf(req).toByteArray()
+}
+
--- src/main/kotlin/sink/KafkaSink.kt
+++ src/main/kotlin/sink/KafkaSink.kt
@@ -4,21 +4,7 @@ import franz.ProducerBuilder
 import franz.producer.Producer
 import leia.logic.IncomingRequest
 import leia.logic.SinkDescription
-import se.zensum.leia.config.Format
-import io.ktor.http.HttpMethod as KtorHttpMethod
-import se.zensum.webhook.PayloadOuterClass
 
-typealias HttpMethod = PayloadOuterClass.HttpMethod
-fun convertMethod(method: KtorHttpMethod): PayloadOuterClass.HttpMethod = when(method) {
-    KtorHttpMethod.Put -> HttpMethod.PUT
-    KtorHttpMethod.Patch -> HttpMethod.PATCH
-    KtorHttpMethod.Delete -> HttpMethod.DELETE
-    KtorHttpMethod.Get -> HttpMethod.GET
-    KtorHttpMethod.Head -> HttpMethod.HEAD
-    KtorHttpMethod.Post -> HttpMethod.POST
-    KtorHttpMethod.Options -> HttpMethod.OPTIONS
-    else -> throw IllegalArgumentException("Method ${method.value} is not supported")
-}
 
 private fun mkProducer(servers: String?) =
     ProducerBuilder.ofByteArray
@@ -28,40 +14,6 @@ private fun mkProducer(servers: String?) =
                 it.option("boostrap.servers", servers)
             } else it
         }.create()
-
-
-fun toProtoPair(pair: Pair<String, String>): PayloadOuterClass.MultiMap.Pair {
-    val (k, v) = pair
-    return PayloadOuterClass.MultiMap.Pair.newBuilder().also {
-        it.key = k
-        it.value = v
-    }.build()
-}
-
-fun parseMap(valuesMap: Map<String, List<String>>): PayloadOuterClass.MultiMap {
-    return PayloadOuterClass.MultiMap.newBuilder().apply {
-        valuesMap.entries.asSequence()
-            .flatMap { (k, vs) -> vs.map { v -> k to v }.asSequence() }
-            .map { toProtoPair(it) }
-            .forEach { addPair(it) }
-    }.build()
-}
-
-suspend fun createPayloadProtobuf(req: IncomingRequest): PayloadOuterClass.Payload =
-    req.run {
-        PayloadOuterClass.Payload.newBuilder().also {
-            it.path = path
-            it.method = convertMethod(method)
-            it.headers = parseMap(headers)
-            it.queryString = queryString
-            it.body = readBody().toString(Charsets.UTF_8)
-            it.flakeId = requestId
-        }.build()
-    }
-private suspend fun Format.generateBody(req: IncomingRequest) : ByteArray = when (this) {
-    Format.RAW_BODY -> req.readBody()
-    Format.PROTOBUF -> createPayloadProtobuf(req).toByteArray()
-}
 
 private class KafkaSink(
     private val producer: Producer<String, ByteArray>,
