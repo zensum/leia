74e8ce4038fec4aab8ccd966e05421011ab440c3
Anton Österberg <anton.osterberg@gmail.com>
2018-09-06 20:05:08 +0200

    Change users in test case.

--- src/test/kotlin/auth/AuthProviderSpecTest.kt
+++ src/test/kotlin/auth/AuthProviderSpecTest.kt
@@ -13,9 +13,9 @@ class AuthProviderSpecTest {
             isDefault = false,
             options = mapOf(
                 "basic_auth_users" to mapOf(
-                    "good-cash-se" to "3df8232404be55d2d6b85be79b99316e028676b69efa2ce91d9094dd18dd3502",
-                    "collector-se" to "c45441cb346b69e247711e9c86405ce9de964bb89c68ad02b8eb2f55912c58ba",
-                    "lendify-se" to "abb9eca0af80e9f321af97b2952ca035edfdc63d5118299e32047156339955b3"
+                    "alice" to "3df8232404be55d2d6b85be79b99316e028676b69efa2ce91d9094dd18dd3502",
+                    "bob" to "c45441cb346b69e247711e9c86405ce9de964bb89c68ad02b8eb2f55912c58ba",
+                    "eve" to "abb9eca0af80e9f321af97b2952ca035edfdc63d5118299e32047156339955b3"
                 )
             )
         )
