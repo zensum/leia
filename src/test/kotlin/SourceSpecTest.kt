448838772cebcc9cd7a2684ef4114922f73ded54
Anton Österberg <anton.osterberg@gmail.com>
2018-09-06 09:17:39 +0200

    "Fixed" bug in test case (no it fails) :crying_cat_face:

--- src/test/kotlin/SourceSpecTest.kt
+++ src/test/kotlin/SourceSpecTest.kt
@@ -1,7 +1,15 @@
+import auth.JwkAuth
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import leia.ssr
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import se.zensum.leia.auth.AuthProvider
+import se.zensum.leia.auth.AuthProviderFactory
+import se.zensum.leia.auth.AuthProviderSpec
+import se.zensum.leia.auth.BasicAuth
+import se.zensum.leia.auth.DefaultAuthProviderFactory
 import se.zensum.leia.config.Format
 import se.zensum.leia.config.SourceSpec
 
@@ -14,11 +22,11 @@ class SourceSpecTest {
                 mapOf<String, Any>(
                     "path" to "/",
                     "topic" to "topic",
-                    "format" to Format.PROTOBUF,
+                    "format" to "proto",
                     "verify" to true,
-                    "allowedMethods" to listOf(HttpMethod.Post),
+                    "allowedMethods" to listOf("POST"),
                     "corsHosts" to emptyList<String>(),
-                    "response" to HttpStatusCode.Accepted,
+                    "response" to HttpStatusCode.Accepted.value,
                     "authenticateUsing" to listOf("no_auth")
                 )
             )
