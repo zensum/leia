74059fb6caafe4a103ae649408056f8bf0f7f6d9
Paweł Nadolski <pawel.nadolski@gmail.com>
2019-02-19 14:17:39 +0100

    Add Google Cloud Pub/Sub sink (gpubsub)

--- src/main/kotlin/sink/DefaultSinkProviderFactory.kt
+++ src/main/kotlin/sink/DefaultSinkProviderFactory.kt
@@ -4,10 +4,11 @@ import se.zensum.leia.config.SinkProviderSpec
 
 class DefaultSinkProviderFactory : SinkProviderFactory {
     override fun create(spec: SinkProviderSpec): SinkProvider? =
-        when(spec.type.toLowerCase()) {
+        when (spec.type.toLowerCase()) {
             "null" -> NullSinkProvider
             "kafka" -> KafkaSinkProvider(spec.options["host"] as String?)
-            "redis" -> RedisSinkProvider(spec.options["host"] as String?, (spec.options["port"] as String?) ?.toIntOrNull())
+            "redis" -> RedisSinkProvider(spec.options["host"] as String?, (spec.options["port"] as String?)?.toIntOrNull())
+            "gpubsub" -> GPubSubSinkProvider(spec.options["projectId"] as String?)
             "always-error" -> AlwaysErrorSinkProvider(spec.options["message"] as String)
             else ->
                 throw RuntimeException("No sinkProvider matching type ${spec.type}")
